08288c616c335e6d5c3298a7c8faa6aa8e3ebc0d
joao <joao@gimo.co.uk>
2013-10-07 17:26:01 +0100

    switched to argparse

--- main.py
+++ main.py
@@ -1,150 +1,62 @@
 #!/usr/bin/python
 
 import sys
-from sys import argv
-
-import config
+import argparse
+from sserver import SServerList
 
 configfile = "~/.remotes_config"
 
-EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
 
-def markMountedServers( sl ):
-   # read mtab into memory
-   mtab = open( "/etc/mtab", "r" )
-   mtabText = mtab.read()
-   mtab.close()
+parser = argparse.ArgumentParser(description='Control ssh endpoints.')
+parser.add_argument('-a', '--all', dest='mount_all', action='store', nargs='?', const='', metavar='FILTER',
+                   help='Mount all endpoints matching the (optional) filter.')
+parser.add_argument('-n', '--none', dest='unmount_all', action='store', nargs='?', const='', metavar='FILTER',
+                   help='Unmount all endpoints matching the (optional) filter.')
+parser.add_argument('-l', '--list', dest='list', action='store', nargs='?', const='', metavar='FILTER',
+                   help='List endpoints.')
+parser.add_argument('--format', dest='list_format', action='store', metavar='FORMAT',
+                   choices=['normal','verbose'], default='normal',
+                   help='Format used to print endpoints.')
+parser.add_argument('--state', dest='filter_state', action='store', metavar='STATE',
+                   choices=['mounted','unmounted','any'], default='any',
+                   help='Filter printed endpoints by state.')
+parser.add_argument('-s', '--ssh', dest='ssh', action='store', nargs=1, metavar='ENDPOINT_NAME',
+                   help='SSH into endpoint.')
+parser.add_argument('-m', '--mount', dest='mount', action='store', nargs=1, metavar='ENDPOINT_NAME',
+                   help='Mount endpoints.')
+parser.add_argument('-u', '--unmount', dest='unmount', action='store', nargs=1, metavar='ENDPOINT_NAME',
+                   help='Unmount endpoints.')
 
-   # check which servers are in there
-   for s in sl:
-      s.mounted = mtabText.find( s.username + "@" + s.ip + ":" + s.remotepath) is not -1
+args = parser.parse_args()
+print args
 
-def printlist( namefilter='', mounted='any', printmode='' ):
-   """ prints the list of servers.
-      namefilter - used to filter the servers by name. Only those that have the filter string on
-      on their name are printed
-      mounted - used to filter the servers by their state.
-         * any - all servers are printed
-         * yes - only mounted servers are printed
-         * no - only unmounted servers are printed
-      print mode - prints details if 'verbose', otherwise it just prints the server name """
+servers = SServerList()
+servers.add_from_config( configfile )
 
-   for s in serverList:
-      if namefilter in s.name:
+if args.list is not None:
+   servers.print_list( args.list, args.filter_state, args.list_format )
+elif args.mount_all is not None:
+   for server in servers.find_all(partial_match=args.mount_all):
+      server.mount()
+      server.Print()
+elif args.unmount_all is not None:
+   for server in servers.find_all(partial_match=args.unmount_all):
+      server.unmount()
+      server.Print()
+elif args.ssh:
+   server = servers.find_all(exact_match=args.ssh[0])[0]
+   if server is not None:
+      server.ssh()
+elif args.mount:
+   server = servers.find_all(exact_match=args.mount[0])[0]
+   if server is not None:
+      server.mount()
+elif args.unmount:
+   server = servers.find_all(exact_match=args.unmount[0])[0]
+   if server is not None:
+      server.unmount()
+else:
+   sys.exit(EXIT_FAILURE)
 
-         if mounted=='yes' and s.mounted is False:
-            continue
-
-         if mounted=='no' and s.mounted is True:
-            continue
-
-         if printmode == "verbose":
-            s.PrintDetails()
-         else:
-            s.Print()
-
-def printOptions():
-   print "USAGE: " + sys.argv[0] + " <command> <rremote>"
-   print
-   print "Commands:"
-   print "    list [filter]         - List all remotes, indicating which are mounted and filtering by filter"
-   print "    listm [filter]        - List all mounted remotes and filtering by filter"
-   print "    listu [filter]        - List all unmounted remotes and filtering by filter"
-   print "    listdetails [filter]  - List all remotes, indicating which are mounted. With full details."
-   print "    ssh                   - SSH into the specified remote"
-   print "    mount                 - Mount the specified remote (if not already mounted)"
-   print "    unmount                - Unmount the specified remote (if mounted)"
-   print "    all                   - Mount all unmounted remotes"
-   print "    none                  - Unmount all mounted remotes"  
-
-minarguments = { "list"         : 2,
-               "listm"        : 2,
-               "listu"        : 2,
-               "listdetails"  : 2,
-               "none"         : 2,
-               "all"          : 2,
-               "mount"        : 3,
-               "unmount"      : 3,
-               "ssh"          : 3
-             }
-
-if len(argv) is 1 or sys.argv[1] == "help":
-   printOptions()
-   sys.exit(EXIT_SUCCESS)
-
-serverList = config.ReadConfigIntoList( configfile )
-markMountedServers(serverList)
-
-
-###
-### check argument validity
-###
-
-option = sys.argv[1]
-
-try:
-   if minarguments[option] > len(argv):
-      printOptions()
-except KeyError:
-   printOptions()
-
-###
-### global options 
-###
-
-if "list" in option:
-
-   namefilter = '' if len(argv)==2 else argv[2]
-   printmode = 'verbose' if option == "listdetails" else ''
-   mounted = 'yes' if option == "listm" else ( 'no' if option == "listu" else 'any' )
-
-   printlist( namefilter, mounted, printmode )
-   sys.exit(EXIT_SUCCESS)
-
-if "none" in option:
-   namefilter = '' if len(argv)==2 else argv[2]
-   
-   for s in serverList:
-      if namefilter in s.name:
-         s.unmount()
-         s.Print()
-
-   sys.exit(EXIT_SUCCESS)
-
-if "all" in option:
-   namefilter = '' if len(argv)==2 else argv[2]
-   
-   for s in serverList:
-      if namefilter in s.name:
-         s.mount()
-         s.Print()
-
-   sys.exit(EXIT_SUCCESS)
-
-###
-### server options 
-###
-
-if len(argv) == 3 :
-
-   serverFound = False
-
-   servername = sys.argv[2]
-   for s in serverList:
-      if s.name != servername:
-         continue
-
-      serverFound = True
-
-      if option == "unmount":
-            s.unmount()
-
-      if option == "mount":
-            s.mount()
-
-      if option == "ssh":
-            s.ssh()
-
-   if serverFound is not True:
-      print "Server not found."
+sys.exit(0)
--- sserver.py
+++ sserver.py
@@ -1,4 +1,5 @@
 import os
+from configobj import ConfigObj
 
 def ssh( user, ip, port ):
    os.system( "ssh -p " + port + " " + user + "@" + ip )
@@ -77,3 +78,69 @@ class SServer():
       ssh( self.username, self.ip, self.port )
       return 1
 
+class SServerList():
+   def __init__( self ):
+      self.servers = []
+
+   def mark_mounted( self ):
+      """ Run trought the servers and mark as mounted those that are found in /etc/mtab. """
+      # read mtab into memory
+      mtab = open( "/etc/mtab", "r" )
+      mtabText = mtab.read()
+      mtab.close()
+
+      # check which servers are in there
+      for s in self.servers:
+         s.mounted = mtabText.find( s.username + "@" + s.ip + ":" + s.remotepath) is not -1
+
+   def add_from_config( self, filename ):
+         
+      final_path = os.path.expanduser(filename)
+      config = ConfigObj( final_path )
+
+      for c in config:
+         ip = config[c]["ip"]
+         port = config[c]["port"]
+         user = config[c]["user"]
+         remotepath = config[c]["remotepath"]
+         mountpoint = config[c]["mountpoint"]
+
+         self.servers.append( SServer( c, mountpoint, user, ip, remotepath, port ) )
+
+      self.mark_mounted()
+
+   def print_list( self, namefilter='', state='any', printmode='' ):
+      """ prints the list of servers.
+         namefilter - used to filter the servers by name. Only those that have the filter string on
+         on their name are printed
+         state- used to filter the servers by their state.
+            * any - all servers are printed
+            * mounted - only mounted servers are printed
+            * unmounted - only unmounted servers are printed
+         print mode - prints details if 'verbose', otherwise it just prints the server name """
+
+      for s in self.servers:
+         if namefilter in s.name:
+
+            if state=='mounted' and s.mounted is False:
+               continue
+
+            if state=='unmounted' and s.mounted is True:
+               continue
+
+            if printmode == "verbose":
+               s.PrintDetails()
+            else:
+               s.Print()
+
+   def find_all( self, exact_match=None, partial_match=None ):
+      """ Get the servers that have a name matching the parameters.
+         If exact_match is provided, returns the ones that match that exact string.
+         Otherwise, returns the servers that have a name including that string. """
+      if exact_match is not None:
+         return [ s for s in self.servers if exact_match == s.name ]
+
+      if partial_match is not None:
+         return [ s for s in self.servers if partial_match in s.name ]
+
+      return []
