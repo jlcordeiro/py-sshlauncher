68c1cebc1c6741dc251aec17ba84e46d2bb2ab8a
joao <joao@gimo.co.uk>
2013-10-08 20:02:46 +0100

    first failed attempt at setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='sshlauncher',
+      version='0.1.0',
+      description='Quickly ssh/mount/unmount into another machine',
+      author='Joao Cordeiro',
+      author_email='jlcordeiro@gmail.com',
+      url='https://github.com/jlcordeiro/py-sshlauncher',
+      platforms=['all'],
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7'
+      ],
+      py_modules=['sshlauncher'],
+      )
