22ac934bee335474dbd36fab674f636246ff70e3
Joao Cordeiro <joao.cordeiro@isoftbet.com>
2020-05-15 23:05:37 +0100

    receive all params to sys call formats as string

--- systemcalls.py
+++ systemcalls.py
@@ -7,7 +7,7 @@ from subprocess import check_call, CalledProcessError
 
 def ssh(suser, sip, sport):
     """ SSH into machine. """
-    command = "ssh -p %d %s@%s" % (int(sport), suser, sip)
+    command = "ssh -p %s %s@%s" % (sport, suser, sip)
     os.system(command)
 
 def sshfs(suser, sip, sport, spath, mountpoint):
@@ -16,7 +16,7 @@ def sshfs(suser, sip, sport, spath, mountpoint):
         code = errno.ENOTDIR
         raise OSError(code, errno.errorcode[code])
 
-    command = "sshfs -C -p %d %s@%s:%s %s" % (int(sport),
+    command = "sshfs -C -p %s %s@%s:%s %s" % (sport,
                                               suser,
                                               sip,
                                               spath,
@@ -26,7 +26,7 @@ def sshfs(suser, sip, sport, spath, mountpoint):
 
 def sftp(suser, sip, sport, spath):
     """ SFTP into machine. """
-    command = "sftp -P%d %s@%s:%s" % (int(sport), suser, sip, spath)
+    command = "sftp -P%s %s@%s:%s" % (sport, suser, sip, spath)
     os.system(command)
 
 
