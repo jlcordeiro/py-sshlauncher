30785742e26e7e2de7695085fc4dc30c3440a39c
Joao Cordeiro <joao.cordeiro@isoftbet.com>
2020-05-16 04:02:20 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sshlauncher',
-      version='0.1.3',
+      version='0.2.0',
       description='Quickly ssh/mount/unmount into another machine',
       author='Joao Cordeiro',
       author_email='jlcordeiro@gmail.com',
