b63c7bdc3353aac9ab7083c318ee9f1e63cf26a2
Joao Cordeiro <joao.cordeiro@isoftbet.com>
2020-05-16 03:57:15 +0100

    refactor away all classes

--- sshlauncher.py
+++ sshlauncher.py
@@ -13,7 +13,10 @@ COMMANDS = {"list":     ("echo \"{NAME} {USER}@{IP}:{PORT} on {MOUNTPOINT}\"", "
             "ssh":      ("ssh -p {PORT} {USER}@{IP}",)
            }
 
-class Endpoint(object):
+
+def run_command(config, command_name, endpoint_name):
+    """ Run a command on the specified endpoint. """
+
     NAME = "name"
     IP = "ip"
     PORT = "port"
@@ -21,21 +24,17 @@ class Endpoint(object):
     RPATH = "remotepath"
     LPATH = "mountpoint"
 
-    """ Class that represents a server. """
-    def __init__(self, name, details):
-        self.details = details
-        self.name = name
+    endpoint_details = config[endpoint_name]
+    cmd = COMMANDS[command_name][0] \
+             .replace("{PORT}", endpoint_details[PORT])   \
+             .replace("{USER}", endpoint_details[USER])   \
+             .replace("{IP}", endpoint_details[IP])       \
+             .replace("{RPATH}", endpoint_details[RPATH]) \
+             .replace("{NAME}", endpoint_name) \
+             .replace("{MOUNTPOINT}", os.path.expanduser(endpoint_details[LPATH]))
 
-    def run(self, action):
-        cmd = COMMANDS[action][0] \
-                 .replace("{PORT}", self.details[self.PORT])   \
-                 .replace("{USER}", self.details[self.USER])   \
-                 .replace("{IP}", self.details[self.IP])       \
-                 .replace("{RPATH}", self.details[self.RPATH]) \
-                 .replace("{NAME}", self.name) \
-                 .replace("{MOUNTPOINT}", os.path.expanduser(self.details[self.LPATH]))
+    os.system(cmd)
 
-        os.system(cmd)
 
 def command_applies_to_all(command_name):
     """
@@ -45,22 +44,6 @@ def command_applies_to_all(command_name):
     """
     return "opt:apply_to_all" in COMMANDS[command_name]
 
-def run(command_name, server_names):
-    CONFIG = ConfigObj(os.path.expanduser('~/.remotes_config'))
-
-    # if a command that should apply to all endpoints
-    if command_applies_to_all(command_name):
-        server_names.sort()
-        for ep in CONFIG:
-            Endpoint(ep, CONFIG[ep]).run(command_name)
-        return
-
-    
-    for ep in server_names:
-        if ep in CONFIG:
-            Endpoint(ep, CONFIG[ep]).run(command_name)
-        else:
-            print("Endpoint {} not found in config.", ep)
 
 
 if __name__ == "__main__":
@@ -68,5 +51,9 @@ if __name__ == "__main__":
         print("usage: {} {list,mount,unmount,ssh,sftp} [ep1, ep2, ...]", sys.argv[0])
         sys.exit(1)
 
-    (command, endpoints) = (sys.argv[1], sys.argv[2:])
-    run(command, endpoints)
+    (command_name, endpoints) = (sys.argv[1], sys.argv[2:])
+    config = ConfigObj(os.path.expanduser('~/.remotes_config'))
+
+    for ep in config:
+        if ep in endpoints or command_applies_to_all(command_name):
+            run_command(config, command_name, ep)
